7c6beb7961a149b0f39a4c9b8862ce8d53856f37
Crealex <alexandre.tomasi80@gmail.com>
2025-11-18 10:51:57 +0100

    start creation class Config

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -3,15 +3,48 @@
 #define CONFIG_HPP
 
 #include <iostream>
+#include <map>
+
+struct site
+{
+	std::string protocol;	 // List of accepted methods for the route (Not sure of name and variabel type)
+	std::string redirection; // HTTP redirection
+	std::string dirRoot;	 // not sure
+	bool dirListing;		 // Enable or disable listening of directory
+	std::string defaultFile; // default file when the request ressource is a directory
+	bool uploadFiles;		 // Enable or disable uploading files from the clients to the server is authorized, and storage location is provided
+	std::string CGI;		 // path of the CGI (maybe need more rule)
+};
 
 class Config
 {
   private:
+	std::string address;					  // IP address or localhost
+	unsigned int port;						  // port (maybe can be a array)
+	std::string errorPage;					  // Path of default error page (maybe needed error code)
+	unsigned int maxSize;					  // max size bodies request
+	std::map<std::string, struct site> sites; // key = site name, value = struct of element
+
   public:
-	Config();
-	Config(const Config &cpy);
+	Config(std::string path);
 	~Config();
-	Config &operator=(const Config &rhs);
+
+	// GETTER
+	std::string getAddress();
+	const unsigned int getPort();
+	const std::string getErrorPage();
+	const unsigned int getMaxSize();
+	const
+
+		// GETTER FROM struct site
+		std::string getProtocol(std::string siteName);
+	const std::string getRedirection(std::string siteName);
+	const std::string getDirRoot(std::string siteName);
+	const bool getDirListing(std::string siteName);
+	const std::string getDefaultFile(std::string siteName);
+	const bool getUploadFiles(std::string siteName);
+	const std::string getCGI(std::string siteName);
+	const
 };
 
-#endif // !CONFIG_HPP
+#endif
--- srcs/Config.cpp
+++ srcs/Config.cpp
@@ -2,14 +2,10 @@
 #include "../includes/Config.hpp"
 #include "../includes/colors.hpp"
 
-Config::Config()
+Config::Config(std::string path)
 {
+	// parsing config file
 	std::cout << GREEN << "Default Config constructor called" << RESET << std::endl;
-}
-
-Config::Config(const Config &cpy)
-{
-	std::cout << BLUE << "Copy constructor called" << RESET << std::endl;
 }
 
 Config::~Config()
@@ -17,7 +13,7 @@ Config::~Config()
 	std::cout << RED << "Destructor called" << RESET << std::endl;
 }
 
-Config &Config::operator=(const Config &rhs)
+std::string Config::getAddress() const
 {
-	return (*this);
+	return (this->address);
 }
