ee668a16d25c6ce07eb9595e93b079cc5dd5c178
Kenviro <ptigars2004@gmail.com>
2025-11-24 11:26:57 +0100

    add function doc

--- srcs/parsing_config/parseElt.cpp
+++ srcs/parsing_config/parseElt.cpp
@@ -5,6 +5,12 @@
 #include "../../includes/structParse.hpp"
 #include "../../includes/Config.hpp"
 
+/**
+ * @brief Check if there is a semicolon at the end of the string 
+ * and erase it, if not throw an error
+ * 
+ * @param str 
+ */
 static void removeSemiColon(std::string &str)
 {
 	std::string::iterator ite = str.end();
@@ -18,6 +24,13 @@ static void removeSemiColon(std::string &str)
 	}
 }
 
+/**
+ * @brief Check if there is exactly two element in the string.
+ * the different element are separate with space.
+ * if the number of element is not 2 throw an error
+ * 
+ * @param str 
+ */
 static void twoElement(std::string str)
 {
 	std::stringstream ss(str);
@@ -33,6 +46,12 @@ static void twoElement(std::string str)
 	}
 }
 
+/**
+ * @brief Get the Site Name object
+ * 
+ * @param sName 
+ * @return std::string 
+ */
 static std::string getSiteName(std::string sName)
 {
 	std::stringstream ss(sName);
@@ -45,6 +64,16 @@ static std::string getSiteName(std::string sName)
 	return str;
 }
 
+/**
+ * @brief Create a Method object.
+ * Create the map and fill it with the our 3 method/bool pair.
+ * the bool are by default false and if the method is found in the string
+ * put the right bool at true. if an unkown method is found in the string
+ * return an error
+ * 
+ * @param str 
+ * @return std::map<std::string, bool> 
+ */
 static std::map<std::string, bool> createMethod(std::string str)
 {
 	std::map<std::string, bool> method;
@@ -76,6 +105,14 @@ static std::map<std::string, bool> createMethod(std::string str)
 	return method;
 }
 
+/**
+ * @brief Create a Redirection object.
+ * Get each element after the first in the string param
+ * and push them in the returned vector
+ * 
+ * @param str 
+ * @return std::vector<std::string> 
+ */
 static std::vector<std::string> createRedirection(std::string str)
 {
 	removeSemiColon(str);
@@ -117,6 +154,13 @@ static std::string returnRoot(std::string str)
 	return ret;
 }
 
+/**
+ * @brief Return the default file path and check if 
+ * it's a valid path, if not throw an error
+ * 
+ * @param str 
+ * @return std::string 
+ */
 static std::string returnDefaultFile(std::string str)
 {
 	twoElement(str);
@@ -141,6 +185,14 @@ static std::string returnDefaultFile(std::string str)
 	return ret;
 }
 
+/**
+ * @brief Check if there is exactly 2 element
+ * and return is the second element is "true" or "false"
+ * 
+ * @param str 
+ * @return true 
+ * @return false 
+ */
 static bool returnBool(std::string str)
 {
 	twoElement(str);
@@ -155,6 +207,15 @@ static bool returnBool(std::string str)
 	throw std::invalid_argument(error + str);
 }
 
+/**
+ * @brief Check if the fullString param end with the
+ * ending string, return true if yes and false if not
+ * 
+ * @param fullString 
+ * @param ending 
+ * @return true 
+ * @return false 
+ */
 static bool endsWith(const std::string& fullString, const std::string& ending)
 {
     if (ending.size() > fullString.size())
@@ -165,6 +226,13 @@ static bool endsWith(const std::string& fullString, const std::string& ending)
                               ending.size(), ending) == 0;
 }
 
+/**
+ * @brief Check if the path of the CGI end with the right
+ * codec for a CGI (.php, .py, etc)
+ * 
+ * @param str 
+ * @return std::string 
+ */
 static std::string returnCGI(std::string str)
 {
 	twoElement(str);
@@ -190,6 +258,14 @@ static std::string returnCGI(std::string str)
 	return ret;
 }
 
+/**
+ * @brief Create a Struct Site object. 
+ * Get each element of the siteParse struct, check them
+ * and parse them correctly in the return site
+ * 
+ * @param site 
+ * @return site 
+ */
 static site createStructSite(siteParse site)
 {
 	struct site ret;
