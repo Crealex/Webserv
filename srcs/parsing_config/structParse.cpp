220460ce98799f79a459f9f282ea1bd47ee41d86
neoo-n <dana.vauthey@gmail.com>
2025-11-24 14:51:25 +0100

    done parsing

--- srcs/parsing_config/structParse.cpp
+++ srcs/parsing_config/structParse.cpp
@@ -17,7 +17,7 @@ static std::string	removeSemicolon(std::string str)
 
 	indexSemicolon = str.rfind(';');
 	if (indexSemicolon == std::string::npos || indexSemicolon != str.size() - 1)
-		throw std::invalid_argument(RED "Invalid argument" RESET);
+		throw std::invalid_argument(RED "Invalid argument : semi colon" RESET);
 	result = str.substr(0, str.size() - 1);
 	return (result);
 }
@@ -59,13 +59,15 @@ static vecAddPort	parseAddressPort(std::vector<std::string> data)
 		std::stringstream	ss;
 		infos = getValue(data[i]);
 		if (infos.size() != 2)
-			throw std::invalid_argument(RED "Invalid argument" RESET);
+			throw std::invalid_argument(RED "Invalid argument : address port" RESET);
 		temp = removeSemicolon(infos[1]);
 		colon = temp.find(':');
 		if (colon == std::string::npos || colon != temp.rfind(':'))
-			throw std::invalid_argument(RED "Invalid argument" RESET);
+			throw std::invalid_argument(RED "Invalid argument : address port 1" RESET);
 		ss << temp.substr(colon + 1, temp.size() - colon - 1);
 		ss >> port;
+		if (port > 65535)
+			throw std::invalid_argument(RED "Invalid argument : name server" RESET);
 		result.push_back(std::make_pair(temp.substr(0, colon), port));
 	}
 	return (result);
@@ -89,7 +91,7 @@ std::vector<serverData>	parseServer(std::vector<hostname> data)
 	{
 		infos = getValue(data[i].serverName);
 		if (infos.size() != 2)
-			throw std::invalid_argument(RED "Invalid argument" RESET);
+			throw std::invalid_argument(RED "Invalid argument : name server" RESET);
 		tempData.name = removeSemicolon(infos[1]);
 		tempData.addressPort = parseAddressPort(data[i].addressPort);
 		result.push_back(tempData);
@@ -100,15 +102,17 @@ std::vector<serverData>	parseServer(std::vector<hostname> data)
 std::vector<unsigned int>	getCode(std::vector<std::string> data)
 {
 	unsigned int				sizeData;
-	std::stringstream			ss;
 	unsigned int				temp;
 	std::vector<unsigned int>	result;
 
 	sizeData = data.size();
 	for (unsigned int i = 1; i < sizeData - 1; i++)
 	{
+		std::stringstream			ss;
 		ss << data[i];
 		ss >> temp;
+		if (temp < 100 || temp > 599)
+			throw std::invalid_argument(RED "Invalid argument : name server" RESET);
 		result.push_back(temp);
 	}
 	return (result);
@@ -127,7 +131,7 @@ std::vector<errorData>	parseError(std::vector<std::string> data)
 	{
 		infos = getValue(data[i]);
 		if (infos.size() < 3)
-			throw std::invalid_argument(RED "Invalid argument" RESET);
+			throw std::invalid_argument(RED "Invalid argument : error path" RESET);
 		tempData.path = removeSemicolon(infos[infos.size() - 1]);
 		tempData.code = getCode(infos);
 		result.push_back(tempData);
@@ -144,36 +148,8 @@ unsigned int	parseMaxSize(std::string data)
 
 	infos = getValue(data);
 	if (infos.size() != 2)
-		throw std::invalid_argument(RED "Invalid argument" RESET);
+		throw std::invalid_argument(RED "Invalid argument : max size" RESET);
 	ss << removeSemicolon(infos[1]);
 	ss >> result;
 	return (result);
 }
-
-int main()
-{
-	hostname	h1;
-	h1.serverName = "hiii";
-	h1.addressPort.push_back("localhost:8080");
-	h1.addressPort.push_back("localhost:4040");
-	hostname	h2;
-	h2.serverName = "Hello World";
-	h2.addressPort.push_back("default:8080");
-	h2.addressPort.push_back("default:4040");
-	std::vector<hostname> s;
-	s.push_back(h1);
-	s.push_back(h2);
-	std::vector<serverData>	sResult;
-	sResult = parseServer(s);
-	for (std::vector<serverData>::iterator it = sResult.begin(); it != sResult.end(); it++)
-	{
-		std::cout << "Server : " << *it.name << std::end;
-		for (std::vector<vecAddPort>::iterator it1 = it.addressPort.begin(); it1 != it.addressPort.end(); it1++)
-		{
-			std::cout << *it.*it1.first << ":" << *it.*it1.second << std::endl;
-
-		}
-	}
-	return (0);
-}
-
