3046a76a526729e7978b7dc865323306f5150b5e
Crealex <Alexandre.tomasi80@gmail.com>
2025-11-27 19:04:15 +0100

    continue and make some tests

--- srcs/request/Get.cpp
+++ srcs/request/Get.cpp
@@ -1,7 +1,11 @@
 #include "../../includes/requests/Get.hpp"
 #include "../../includes/colors.hpp"
 #include "../../includes/requests/Request.hpp"
+#include <cstdio>
+#include <ctime>
 #include <fstream>
+#include <string>
+#include <sys/stat.h>
 
 Get::Get(Request requ): Methods(requ), _accept(requ._accept), _userAgent(requ._userAgent)
 {
@@ -10,33 +14,84 @@ Get::Get(Request requ): Methods(requ), _accept(requ._accept), _userAgent(requ._u
 
 // *** Create response
 
+//INFO: Juste pour avoir une fonctions mais sera porbablement pas comme ça
 static std::string sendError(unsigned int code)
 {
 	(void)code;
 	return ("Heu y a un bleme ma guele");
 }
 
-static bool addContentType(std::string *resp, std::string accept)
+static std::string findMimeType(struct stat fileData)
+{
+	std::cout << fileData.st_mode << std::endl;
+	return ("test/html");
+}
+
+static bool addStartLine(std::string *resp, std::string protocol, unsigned int code)
+{
+	return (true);
+}
+
+//	Content-Type: text/html; charset=UTF-8
+static bool addContentType(std::string *resp, std::string accept, struct stat fileData)
 {
 	std::string contentType;
 
-	// detecte le type de contenu demandé
+	// detecter le type de contenu demandé
+	contentType = findMimeType(fileData);
 	if (contentType != accept && accept != "*/*")
 		return (false);
 	return (true);
 }
 
+static bool addDate(std::string *resp)
+{
+	std::tm time;
+	return (true);
+}
+
+static bool addLastModif(std::string *resp, std::string pathTarget)
+{
+	struct stat buff;
+
+	stat(pathTarget.c_str(), &buff);
+	return (true);
+}
+
+static bool addContentLenght(std::string *resp, std::string file)
+{
+	unsigned int size;
+	size = file.size();
+	return (true);
+}
+
+static bool addBody(std::string *resp, std::string file)
+{
+	return (true);
+}
+
 const std::string Get::createResponse()
 {
-	std::string resp;
-	std::ifstream target;
+	std::string		resp;
+	std::ifstream	target;
+	std::string		file;
+	struct stat		fileData;
 	
-	if (!addContentType(&resp, this->_accept))
-		return (sendError(666));
 	target.open(this->_host + this->_location);
 	if (!target.is_open())
 		return (sendError(666));
-	
+	stat((this->_host + this->_location).c_str(), &fileData);
+	std::getline(target, file, '\0');
+	if (!addContentType(&resp, this->_accept, fileData))
+		return (sendError(666));
+	if (!addDate(&resp))
+		return (sendError(666));
+	if (!addContentLenght(&resp, file))
+		return (sendError(666));
+	if (!addBody(&resp, file))
+		return (sendError(666));
+	if (!addStartLine(&resp, this->_protocol, 200))
+		return (sendError(666));
 
 	return (resp);
 }
