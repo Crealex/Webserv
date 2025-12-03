e4487a688d6e75e6bd2d4b1180ddd15928c231f8
Crealex <alexandre.tomasi80@gmail.com>
2025-12-03 11:23:06 +0100

    ascascas

--- includes/requests/methodsClass.hpp
+++ includes/requests/methodsClass.hpp
@@ -1,32 +1,31 @@
 #ifndef METHODS_CLASS_HPP
 
-# define METHODS_CLASS_HPP
-# include "../includes.hpp"
+#define METHODS_CLASS_HPP
+#include "../includes.hpp"
 
 struct Request;
 
-class Methods {
-	protected:
-		std::string		_method;
-		std::string		_location;
-		std::string		_protocol;
-		std::string		_host;
-		Methods(Request requ);
-	public:
-		virtual const std::string createResponse() = 0;
+class Methods
+{
+  protected:
+	std::string _method;
+	std::string _location;
+	std::string _protocol;
+	std::string _host;
+	Methods(Request requ);
 
-	
+  public:
+	virtual const std::string createResponse() = 0;
 };
 
 // addLineToResp.cpp
 
-bool	addStartLine(std::string *resp, std::string protocol, unsigned int code, std::string mess);
-bool	addContentType(std::string *resp, std::string accept, std::string file);
-bool	addDate(std::string *resp);
-bool	addLastModif(std::string *resp, std::string pathTarget);
-bool	addContentLenght(std::string *resp, std::string file);
-bool	addBody(std::string *resp, std::string file);
-bool	addLocation(std::string *resp, std::string host, std::string location);
+bool addStartLine(std::string *resp, std::string protocol, unsigned int code, std::string mess);
+bool addContentType(std::string *resp, std::string accept, std::string file);
+bool addDate(std::string *resp);
+bool addLastModif(std::string *resp, std::string pathTarget);
+bool addContentLenght(std::string *resp, std::string file);
+bool addBody(std::string *resp, std::string file);
+bool addLocation(std::string *resp, std::string host, std::string location);
 
 #endif
-
