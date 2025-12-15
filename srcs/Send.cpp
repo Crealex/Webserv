5ecb15d5d38ff396e51aeaa4f601bf7cb63edffa
Kenviro <ptigars2004@gmail.com>
2025-12-15 14:30:40 +0100

    send is done we wait now

--- srcs/Send.cpp
+++ srcs/Send.cpp
@@ -1,8 +1,26 @@
 #include "../includes/includes.hpp"
 #include "../includes/Socket.hpp"
 #include "../includes/SocketData.hpp"
+#include "request/getMethods.cpp"
 
-void sendResponse(unsigned int socket, Config conf)
+void sendResponse(unsigned int socket, Config conf, char *buff)
 {
+	Methods *request;
+	std::string response;
+
+	try
+	{
+		request = createMethod(buff);
+		response = request->createResponse();
+		delete request;
+	}
+	catch(const ResponseError& e)
+	{
+		response = e.createResponse();
+	}
 	
+	while (send(socket, response.c_str(), response.size(), 0) == -1)
+	{
+		std::cout << RED << "send failed, retry in processing" << RESET << std::endl;
+	}
 }
