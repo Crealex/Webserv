721cac4058c6e527aaf72d910aacb2aa6dce66b4
Crealex <Alexandre.tomasi80@gmail.com>
2025-12-17 11:32:55 +0100

    miss branch

--- www/danalexian/script.js
+++ www/danalexian/script.js
@@ -1,3 +1,10 @@
-function checkPostMethod() {
+document.querySelectorAll(".card button").forEach((btn) =>
+  btn.addEventListener("click", async () => {
+    const method = btn.dataset.method;
+    const endpoint = btn.dataset.endpoint;
+    const card = btn.closest(".card");
+    const resultSpan = card.querySelector(".result");
 	
-}
+	get
+  }),
+);
