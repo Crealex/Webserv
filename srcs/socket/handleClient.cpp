00ccdc242433c3affba7a35a51d0a877999b308a
Crealex <Alexandre.tomasi80@gmail.com>
2026-01-16 16:46:21 +0100

    caca

--- srcs/socket/handleClient.cpp
+++ srcs/socket/handleClient.cpp
@@ -141,6 +141,7 @@ void	handleClient(std::vector<Socket *> &sockets, std::vector<Server> servers, E
 		if (events[indexEvent].events == (EPOLLIN|EPOLLOUT) && isClientSocket(events[indexEvent].data.fd, clients, idClient))
 		{
 			receiveRequest(clients[idClient]);
+			std::cout << "Reuqest before recieveRequest: " << clients[idClient].getBuf() << std::endl;
 		}
 		else if (events[indexEvent].events == EPOLLOUT && clients[idClient].getEndOfFile())
 		{
