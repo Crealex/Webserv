8e13daefaddc454a56845bbb0b76c2ab9175a5a4
Crealex <Alexandre.tomasi80@gmail.com>
2026-02-04 16:33:34 +0100

    added first handling of redirection

--- srcs/request/Get.cpp
+++ srcs/request/Get.cpp
@@ -1,10 +1,12 @@
 #include "../../includes/requests/Get.hpp"
 #include "../../includes/colors.hpp"
 #include "../../includes/requests/Request.hpp"
+#include <cstddef>
 #include <exception>
 #include <fstream>
 #include <string>
 #include <sys/stat.h>
+#include <utility>
 #include <vector>
 #include "../../includes/Server.hpp"
 #include "../../includes/requests/ResponseError.hpp"
@@ -16,30 +18,9 @@ Get::Get(Request requ): Methods(requ), _userAgent(requ.getUserAgent()), _accept(
 
 
 // *** Create response
-
-const std::string Get::createResponse(Server srv)
+//
+static std::string createFileStr(std::ifstream &file)
 {
-	std::string		resp;
-	std::ifstream	file;
-	std::string		path;
-	Request			dataError;
-	std::string		target;
-	
-	dataError.setProtocol(this->_protocol);
-	dataError.setHost(this->_host);
-	dataError.setAccept(this->_accept);
-	dataError.setLocation(this->_location);
-	dataError.setUserAgent(this->_userAgent);
-
-	target = findTarget(this->_location, srv.getLocations(), dataError, "GET");
-	// TODO: Verif si target est un file ou un dossier ou une redirection;
-	path = srv.getRoot() + "/" + target; // TODO: Peut-etre retirer le /
-	std::cout << "complete path to get: " << path << std::endl;
-	file.open(path.c_str());
-	// *************************
-	if (!file.is_open())
-		throw ResponseError(404, "Not found", dataError);
-
 	file.seekg(0, std::ios::end);
 	size_t size = file.tellg();
 	file.seekg(0, std::ios::beg);
@@ -48,17 +29,81 @@ const std::string Get::createResponse(Server srv)
 	std::string fileStr(buffer, size);
 	delete[] buffer;
 
+	return fileStr;
+}
+
+std::pair<unsigned int, std::string> Get::_findCodeMess(Server srv)
+{
+	std::pair<unsigned int, std::string> ret;
+	int finded = -1;
+
+	std::cout << BOLD << "IN FIND CODE MESS" << RESET << std::endl;
+	for (size_t i = 0; i < srv.getLocations().size(); i++)
+	{
+	  std::cout << RED << "this->location: " << this->_location << ", srv location at " << i << ": " << srv.getLocations().at(i).getPath() << std::endl;;
+		if (this->_location == srv.getLocations().at(i).getPath())
+		{
+			finded = i;
+			break ;
+		}
+	}
+	if (finded == -1)
+		return (std::pair<unsigned int, std::string>(200, "OK"));
+	if (srv.getLocations().at(finded).getReturn().first.empty())
+		return (std::pair<unsigned int, std::string>(200, "OK"));
+	ret.first = srv.getLocations().at(finded).getReturn().second;
+	ret.second = srv.getLocations().at(finded).getReturn().first;
+	return (ret);
+}
+
+const std::string Get::createResponse(Server srv)
+{
+	std::string		resp;
+	std::ifstream	file;
+	std::string		path;
+	Request			dataError;
+	std::string		target;
+	std::string fileStr;
+	std::pair<unsigned int, std::string> codeMess;
+	bool isRedir = 0;
+	
+	dataError.setProtocol(this->_protocol);
+	dataError.setHost(this->_host);
+	dataError.setAccept(this->_accept);
+	dataError.setLocation(this->_location);
+	dataError.setUserAgent(this->_userAgent);
+
+	target = findTarget(this->_location, srv.getLocations(), dataError, "GET");
+	if (target.find("https") > target.size())
+	{
+		path = srv.getRoot() + "/" + target; // TODO: Peut-etre retirer le /
+		file.open(path.c_str());
+		// *************************
+		if (!file.is_open())
+			throw ResponseError(404, "Not found", dataError);
+		fileStr = createFileStr(file);
+	}
+	else
+	{
+		isRedir = 1;
+		path = target;
+	}
+	std::cout << "complete path to get: " << path << std::endl;
+
+	codeMess = _findCodeMess(srv);
 	if (!addContentType(&resp, this->_accept, path))
 		throw ResponseError(406, "Not acceptable", dataError);
 	if (!addDate(&resp))
 		throw ResponseError(500, "Can't add date", dataError);
 	if (!addLastModif(&resp, path))
 		throw ResponseError(500, "can't add last modif", dataError);
+	if (isRedir)
+		addLocation(&resp, codeMess.second);
 	if (!addContentLenght(&resp, path))
 		throw ResponseError(500, "can't add content length", dataError);
 	if (!addBody(&resp, fileStr))
 		throw ResponseError(500, "can't add body", dataError);
-	if (!addStartLine(&resp, this->_protocol, 200, "OK"))
+	if (!addStartLine(&resp, this->_protocol, codeMess.first, codeMess.second))
 		throw ResponseError(500, "can't add start line", dataError);
 
 	std::cout << "end of get" << std::endl;
